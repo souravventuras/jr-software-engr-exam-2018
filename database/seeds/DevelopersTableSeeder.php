d7743bcee06876200504a0c93e3072b93a08fbeb
Eshrat <eshrat.maria@reverb.com.bd>
2018-05-12 11:57:46 +0600

    TaskB

---
+++ database/seeds/DevelopersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DevelopersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+
+        for($i = 0; $i < 100; $i++) {
+            App\Developers::create([
+                'email' => $faker->email
+            ]);
+        }
+    }
+}
