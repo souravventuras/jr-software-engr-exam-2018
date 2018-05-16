d44c065ec4efe4dda9060b43d5591358b6c8af21
ikramhasib007 <ikramhasib007@gmail.com>
2018-05-17 03:42:48 +0600

    remove user factory

--- app/ProgrammingLanguage.php
+++ app/ProgrammingLanguage.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProgrammingLanguage extends Model
 {
+
+    protected $table = 'programming_languages';
+
     public function developers() {
         return $this->belongsToMany('App\Developer');
     }
