51d48c23af36a5c7dfc9269e7aa9a458271a7826
iarafat <miyasinarafat@gmail.com>
2018-10-04 01:10:18 +0600

    Seeder files added for testing

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        
+        $this->call(ProgrammingLanguagesTableSeeder::class);
+        $this->call(LanguagesTableSeeder::class);
+        $this->call(DevelopersTableSeeder::class);
     }
 }
