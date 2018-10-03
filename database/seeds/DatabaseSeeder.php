686d304205d59c34ab04e2c6430be67d690b6472
iarafat <miyasinarafat@gmail.com>
2018-10-04 00:56:54 +0600

    Added programming language seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(LanguagesTableSeeder::class);
+        $this->call(ProgrammingLanguagesTableSeeder::class);
     }
 }
