f4b12b331d4531677a9c5a9f5f7d15884835f1b0
iarafat <miyasinarafat@gmail.com>
2018-10-04 00:54:37 +0600

    Added migrations and language seeder

---
+++ database/seeds/LanguagesTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Language;
+use Illuminate\Database\Seeder;
+
+class LanguagesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Language::class, 50)->create();
+    }
+}
